30617e949bc1861245e264afc149323ace2eaab3
mesong21 <2572551880@qq.com>
2024-06-23 20:20:56 +0800

    更新测试用参数输入

--- src/main.cpp
+++ src/main.cpp
@@ -14,25 +14,20 @@ int main(int argc, char **argv) {
   int tnum = 1;
   Graph *graph = NULL;
   printf("argc: %d\n", argc);
-  if (argc == 3) {
-		tnum = atoi(argv[2]);
-    if (strcmp(argv[1], "t1") == 0) {
-      printf("运行测试\n");
-      strcpy(dir, "/share/triCount/data/test1");
-      print_all = false;
-    } else if (strcmp(argv[1], "mt") == 0) {
-      printf("运行我的小图测试\n");
-      strcpy(dir, "/share/triCount/data/mytest");
-      print_all = true;
-    } else if (strcmp(argv[1], "r") == 0) {
-      printf("运行大数据\n");
-      strcpy(dir, "/share/triCount/data/tmp");
-      print_all = false;
-    } else
-      throw std::runtime_error("参数错误\n");
+  if (argc == 4) {
+		// 需要3个参数，数据目录，线程数，是否打印
+		strcpy(dir, argv[1]);
+    tnum = atoi(argv[2]);
+		int a3 = atoi(argv[3]);
+		if (a3 == 0)
+			print_all = false;
+		else if (a3 == 1)
+			print_all = true;
+		else
+			throw std::runtime_error("参数错误\n");
+    
   } else
     throw std::runtime_error("参数错误\n");
-  
 
   struct timeval start_t, graph_t, calc_t;
   gettimeofday(&start_t, NULL);
